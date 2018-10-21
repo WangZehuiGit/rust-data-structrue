f0ca8b063cfcff5dba6540f7d46d180415f03fbf
Wang Zehui <wzh@DESKTOP-E664FF8.localdomain>
2018-10-21 22:14:09 +0800

    implement Drop and Clone

--- src/vector.rs
+++ src/vector.rs
@@ -1,17 +1,20 @@
+use libc::c_void;
 use support::*;
 use std::mem::size_of;
 
 type	Rank = i32;
 const	DEFAULT_CAPACITY: usize = 8;
 
-pub struct Vector<T> {
+pub struct Vector<T>
+	where T: Clone {
 	size: Rank,
 	capacity: usize,
 	ptr: *mut T
 }
 
 
-pub fn new<T>() -> Vector<T> {
+pub fn new<T>() -> Vector<T>
+	where T: Clone {
 	Vector::<T> {
 		ptr: unsafe_malloc(size_of::<T>() * DEFAULT_CAPACITY) as *mut T,
 		size: 0 as Rank,
@@ -19,8 +22,38 @@ pub fn new<T>() -> Vector<T> {
 	}
 }
 
-impl<T> Vector<T> {
+impl<T> Vector<T>
+	where T: Clone {
 	pub fn capacity(&self) -> usize {
 		self.capacity
+	}
+	pub fn len(&self) -> Rank {
+		self.size
+	}
+	pub fn empty(&self) -> bool {
+		self.size == 0
+	}
+}
+
+impl<T> Clone for Vector<T>
+	where T: Clone {
+	fn clone(&self) -> Self {
+		let new_ptr = unsafe_malloc(self.capacity);
+
+		for i in 0..self.size {
+			unsafe {*((new_ptr as i32 + i) as *mut T) = (*((self.ptr as i32 + i) as *mut T)).clone()}
+		}
+		Vector::<T> {
+			ptr: self.ptr,
+			size: self.size,
+			capacity: self.capacity
+		}
+	}
+}
+
+impl<T> Drop for Vector<T>
+	where T: Clone {
+	fn drop(&mut self) {
+		unsafe_free(self.ptr as *mut c_void);
 	}
 }
--- tests/vector_test.rs
+++ tests/vector_test.rs
@@ -3,9 +3,9 @@ extern crate rust_data_structrue;
 use rust_data_structrue::vector;
 
 #[test]
-fn test_capacity() {
+fn test_base() {
     let v = vector::new::<i32>();
-    if v.capacity() != 32 {
-        panic!();
-    }
+    assert_eq!(v.capacity(), 32);
+    assert_eq!(v.len(), 0);
+    assert_eq!(v.empty(), true);
 }
